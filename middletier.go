dd1c55841a58d2bb1b7dbe6b9fc38fc254b1c743
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-02-26 21:47:13 +0100

    Instrument with appdash opentracing

--- middletier.go
+++ middletier.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
+	"github.com/codegangsta/negroni"
 	"github.com/gocql/gocql"
 	"github.com/gorilla/mux"
 	"github.com/mmcdole/gofeed"
-	"github.com/teeratpitakrat/gokieker"
+	opentracing "github.com/opentracing/opentracing-go"
+	"sourcegraph.com/sourcegraph/appdash"
+	appdashtracer "sourcegraph.com/sourcegraph/appdash/opentracing"
 )
+
+const CtxSpanID = 0
+
+var collector appdash.Collector
 
 var cassandraAddr string
 
@@ -21,32 +28,41 @@ type Subscription struct {
 	Feeds []*gofeed.Feed
 }
 
-func GetFeed(w http.ResponseWriter, req *http.Request) {
-	k := gokieker.BeginFunction()
-	defer k.EndFunction()
+func FetchFeed(w http.ResponseWriter, req *http.Request) {
+	carrier := opentracing.HTTPHeadersCarrier(req.Header)
+	spanCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, carrier)
+	if err != nil {
+		return
+	}
+	span := opentracing.StartSpan("middletier:FetchFeed", opentracing.ChildOf(spanCtx))
+	defer span.Finish()
 
 	vars := mux.Vars(req)
 	user := vars["user"]
-	feedURLs, err := GetUrls(user)
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, span)
+	feedURLs, err := GetUrls(ctx, user)
 	if err != nil {
 		ReturnErrorPage(w, req, err)
 		return
 	}
 	subscription := Subscription{}
 	for _, feedURL := range feedURLs {
-		fp := gofeed.NewParser()
-		feed, _ := fp.ParseURL(feedURL)
+		feed, err := FetchFeedContents(ctx, feedURL)
+		if err != nil {
+			continue
+		}
 		feed.FeedLink = feedURL
 		subscription.Feeds = append(subscription.Feeds, feed)
 	}
 	json.NewEncoder(w).Encode(subscription)
 }
 
-func GetUrls(user string) ([]string, error) {
-	k := gokieker.BeginFunction()
-	defer k.EndFunction()
+func GetUrls(ctx context.Context, user string) ([]string, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "middletier:GetUrls")
+	defer span.Finish()
 
-	session, err := GetCassandraSession()
+	session, err := GetCassandraSession(ctx)
 	if err != nil {
 		return nil, errors.New("Cannot connect to cassandra")
 	}
@@ -66,15 +82,35 @@ func GetUrls(user string) ([]string, error) {
 	return feedURLs, nil
 }
 
-func AddFeed(w http.ResponseWriter, req *http.Request) {
-	k := gokieker.BeginFunction()
-	defer k.EndFunction()
+func FetchFeedContents(ctx context.Context, feedURL string) (*gofeed.Feed, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "middletier:FetchFeedContents")
+	span.SetTag("URL", feedURL)
+	defer span.Finish()
+
+	fp := gofeed.NewParser()
+	feed, err := fp.ParseURL(feedURL)
+	if err != nil {
+		return nil, err
+	}
+	return feed, nil
+}
+
+func Subscribe(w http.ResponseWriter, req *http.Request) {
+	carrier := opentracing.HTTPHeadersCarrier(req.Header)
+	spanCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, carrier)
+	if err != nil {
+		return
+	}
+	span := opentracing.StartSpan("middletier:Subscribe", opentracing.ChildOf(spanCtx))
+	defer span.Finish()
 
 	vars := mux.Vars(req)
 	user := vars["user"]
 	feedURL := req.FormValue("url")
 
-	session, err := GetCassandraSession()
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, span)
+	session, err := GetCassandraSession(ctx)
 	if err != nil {
 		log.Fatal(err)
 		ReturnErrorPage(w, req, err)
@@ -91,15 +127,22 @@ func AddFeed(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func DeleteFeed(w http.ResponseWriter, req *http.Request) {
-	k := gokieker.BeginFunction()
-	defer k.EndFunction()
+func Unsubscribe(w http.ResponseWriter, req *http.Request) {
+	carrier := opentracing.HTTPHeadersCarrier(req.Header)
+	spanCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, carrier)
+	if err != nil {
+		return
+	}
+	span := opentracing.StartSpan("middletier:Unsubscribe", opentracing.ChildOf(spanCtx))
+	defer span.Finish()
 
 	vars := mux.Vars(req)
 	user := vars["user"]
 	feedURL := req.FormValue("url")
 
-	session, err := GetCassandraSession()
+	ctx := context.Background()
+	ctx = opentracing.ContextWithSpan(ctx, span)
+	session, err := GetCassandraSession(ctx)
 	if err != nil {
 		log.Fatal(err)
 		ReturnErrorPage(w, req, err)
@@ -116,7 +159,10 @@ func DeleteFeed(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-func GetCassandraSession() (*gocql.Session, error) {
+func GetCassandraSession(ctx context.Context) (*gocql.Session, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "middletier:GetCassandraSession")
+	defer span.Finish()
+
 	cluster := gocql.NewCluster(cassandraAddr)
 	cluster.Keyspace = "RSS"
 	cluster.Consistency = gocql.Quorum
@@ -134,25 +180,21 @@ func Healthcheck(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	gokieker.StartMonitoring()
-	k := gokieker.BeginFunction()
-	defer k.EndFunction()
+	collector = appdash.NewRemoteCollector("localhost:7701")
+	collector = appdash.NewChunkedCollector(collector)
+	tracer := appdashtracer.NewTracer(collector)
+	opentracing.InitGlobalTracer(tracer)
 
 	cassandraAddr = os.Getenv("CASSANDRA_ADDR")
 	fmt.Println("cassandra addr:", cassandraAddr)
 
-	r := mux.NewRouter()
-	r.HandleFunc("/middletier/rss/user/{user}", GetFeed).Methods("GET")
-	r.HandleFunc("/middletier/rss/user/{user}", AddFeed).Methods("POST")
-	r.HandleFunc("/middletier/rss/user/{user}", DeleteFeed).Methods("DELETE")
-	r.HandleFunc("/healthcheck", Healthcheck)
+	router := mux.NewRouter()
+	router.HandleFunc("/middletier/rss/user/{user}", FetchFeed).Methods("GET")
+	router.HandleFunc("/middletier/rss/user/{user}", Subscribe).Methods("POST")
+	router.HandleFunc("/middletier/rss/user/{user}", Unsubscribe).Methods("DELETE")
+	router.HandleFunc("/healthcheck", Healthcheck)
 
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         ":9191",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
+	n := negroni.Classic()
+	n.UseHandler(router)
+	n.Run(":9191")
 }
